aa6ced2439d756baec6034a641ac743cd2f385bc
Waruni-cpu <warunisandareka963@gmail.com>
2023-11-03 00:02:26 +0530

    Delete mapping

--- src/main/java/lk/task/fullstackbackend/controller/UserController.java
+++ src/main/java/lk/task/fullstackbackend/controller/UserController.java
@@ -32,6 +32,26 @@ public class UserController {
                 .orElseThrow(()->new UserNotFoundException(id));
     }
 
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @DeleteMapping ("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id "+id+ "has been deleted success";
+    }
+
 
 
 }
